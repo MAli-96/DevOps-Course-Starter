411fa79e91fccaa1201f2fc88c3e1a32f89b6ced
Mohammed Ali <mohammedali@Mohammeds-MacBook-Air-2.local>
2022-08-18 14:49:14 +0100

    2nd commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from pickle import FALSE
+from flask import Flask, request
+from flask import render_template
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +11,13 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
+    items = get_items
     return render_template('index.html')
+    return items == items
+
+@app.route('/add', methods=['POST'])
+def add():
+    get_items = request.form.get('title')
+
+    if __name__ == '__main__':
+        app.run()
